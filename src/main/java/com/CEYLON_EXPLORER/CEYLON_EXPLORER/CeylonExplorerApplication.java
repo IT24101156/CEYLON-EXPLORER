b51706c72030da465f0e1d986b6d428a9ef1ac3b
IT24101156 <IT24101156@my.sliit.lk>
2026-03-16 16:04:46 +0530

    add spring boot initializr

---
+++ src/main/java/com/CEYLON_EXPLORER/CEYLON_EXPLORER/CeylonExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.CEYLON_EXPLORER.CEYLON_EXPLORER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeylonExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeylonExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CEYLON_EXPLORER/CEYLON_EXPLORER/CeylonExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CEYLON_EXPLORER.CEYLON_EXPLORER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeylonExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
